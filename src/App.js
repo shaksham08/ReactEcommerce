7ac71d49e850da9e1cf7e0d0835f83b2b3bb03d9
shaksham08 <shakshamsinha@outlook.com>
2021-05-14 17:07:09 +0530

    Added basic layout for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./pages/HomePage/Homepage";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../Components/Directory/Directory";
+import "./Homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
