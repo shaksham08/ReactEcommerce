881f02041d0d3ef50eabc5c980133d6c5341910b
shaksham08 <shakshamsinha@outlook.com>
2021-05-19 08:03:05 +0530

    Fixing bug for counting cart items

--- src/Components/cartIcon/CartIcon.js
+++ src/Components/cartIcon/CartIcon.js
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./CartIcon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
