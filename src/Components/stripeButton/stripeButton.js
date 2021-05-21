9e490aa6c38dd940ea9e4c2e9a97c5afe08a0bd5
shaksham08 <shakshamsinha@outlook.com>
2021-05-21 22:59:59 +0530

    Added stripe test payment to the checkout page

---
+++ src/Components/stripeButton/stripeButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51ItbuISJ6dfRVvp0H2oYz1UR0v26LeZJc8nz8VWuB1R04xBb3ygC1eYkjyCrUWCJtGvsryESOfmjlcXuBcN8HcHA00i7bNOLGb";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
