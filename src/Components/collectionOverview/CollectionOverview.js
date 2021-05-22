ff6acff1b7bc84ed84a022808e01b682b61681e2
shaksham08 <shakshamsinha@outlook.com>
2021-05-22 21:15:50 +0530

    Fixed select collections for preview bug in collections overview component

--- src/Components/collectionOverview/CollectionOverview.js
+++ src/Components/collectionOverview/CollectionOverview.js
@@ -4,20 +4,23 @@ import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../CollectionPreview/CollectionPreview";
 
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 import "./collectionOverview.styles.scss";
 
-const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
-);
+const CollectionsOverview = ({ collections }) => {
+  console.log(collections);
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollection,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
