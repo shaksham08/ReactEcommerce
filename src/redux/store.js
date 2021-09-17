976f43122c32744991024d5c31f23c63a5964a21
shaksham08 <shakshamsinha@outlook.com>
2021-09-17 19:57:22 +0530

    Updating package

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import thunk from "redux-thunk";
-
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./root-reducer";
 
 const middlewares = [thunk];
